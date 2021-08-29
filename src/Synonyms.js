d5947723880220d08a13e1d9dc11bfdc2c39e72c
Katerina <kslezakova12@gmail.com>
2021-08-29 11:17:30 +0200

    added Synonyms into the project

---
+++ src/Synonyms.js
@@ -0,0 +1,19 @@
+
+
+export default function Synonyms(props){
+    if (props.synonyms){
+    return (
+        <div className="Synonyms my-2">
+            {props.synonyms.map(function(synonym,index){
+                return (
+                  <ul>
+                    <li key={index}>{synonym}</li>
+                  </ul>
+                );
+            })}
+        </div>
+    )
+    } else {
+        return null;
+    }
+}
