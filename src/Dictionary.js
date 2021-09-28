d801e64db8303f2bd837c17f39186ddf5602ade9
Katerina <kslezakova12@gmail.com>
2021-09-28 22:33:09 +0200

    changed the amount of images on page

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -27,7 +27,7 @@ export default function Dictionary(props){
 
       let pexelsApiKey = "563492ad6f91700001000001326d54778a5144ff8db5ce6324a7c7ff";
       let pexelsApiUrl =
-        `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+        `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
       let headers = { Authorization: `Bearer ${pexelsApiKey}` };
       axios.get(pexelsApiUrl, { headers: headers}).then(handlePexelsResponse)
     }
