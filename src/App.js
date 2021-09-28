14f27674fd08f5e67d40ba4d1f5d14700b1021d2
Katerina <kslezakova12@gmail.com>
2021-09-28 22:37:24 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">\
+    <div className="App">
       <div className="container">
         <header className="App-header">
       Dictionary
